d85d76b2890b7ed4b71e3d50ff036dc12071ce89
shivansh135 <shivanshmitra53@gmail.com>
2023-10-14 03:23:32 +0530

    first

--- src/component/dearFlip/dearFlip.js
+++ src/component/dearFlip/dearFlip.js
@@ -1,13 +1,8 @@
+/* global DFlip */
+
 import React, { Component } from 'react';
 
 class Flipbook extends Component {
-  constructor() {
-    super();
-    this.state = {
-      scriptsLoaded: false,
-    };
-  }
-
   componentDidMount() {
     this.loadStyles();
     this.loadScripts();
@@ -23,68 +18,53 @@ class Flipbook extends Component {
   }
 
   loadScripts() {
-    const loadScript = (src, type) => {
-      return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.type = type;
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-      });
-    };
+    // Load external scripts
+    const script1 = document.createElement('script');
+    script1.src = '/dflip/js/libs/jquery.min.js';
+    script1.type = 'text/javascript';
+    document.head.appendChild(script1);
 
-    // Load script 1
-    loadScript('/dflip/js/libs/jquery.min.js', 'text/javascript')
-      .then(() => {
-        // Script 1 has loaded successfully
-        // Now load script 2
-        return loadScript('/dflip/js/dflip.min.js', 'text/javascript');
-      })
-      .then(() => {
-        // Script 2 has loaded successfully
-        // Load and initialize script 3
-        const script3 = document.createElement('script');
-        script3.className = 'df-shortcode-script';
-        script3.type = 'text/javascript';
-        script3.innerHTML = `
-          var option_flipbook = {
-            source: '/download?fileId={pdfId}',
-            "overwritePDFOutline": "true",
-            autoEnableOutline: "false",
-            forceFit: true,
-            outline: [],
-            color3DCover: "#fff",
-            hard: "none",
-            shadowOpacity: 0,
-            allControls: "",
-            pageMode: DFLIP.PAGE_MODE.DOUBLE,
-          };
-        `;
+    const script2 = document.createElement('script');
+    script2.src = '/dflip/js/dflip.min.js';
+    script2.type = 'text/javascript';
+    script2.onload = this.initializeFlipbook; // Call initialization function once the script is loaded
+    document.head.appendChild(script2);
 
-        // Call initialization function once script 3 is loaded
-        script3.onload = this.initializeFlipbook;
-        document.head.appendChild(script3);
-      })
-      .then(() => {
-        // All scripts have loaded successfully
-        this.setState({ scriptsLoaded: true });
-      })
-      .catch((error) => {
-        console.error('Script loading failed:', error);
-      });
+    const script3 = document.createElement('script');
+    script3.className = 'df-shortcode-script';
+    script3.type = 'text/javascript';
+    script3.innerHTML = `var option_flipbook = {
+        source:'/download?fileId={pdfId}',
+        "overwritePDFOutline": "true",
+      autoEnableOutline: "false",
+      forceFit:true,
+      outline:[],
+      color3DCover:"#fff",
+      hard: "none",
+      shadowOpacity: 0,
+      allControls: "",
+      pageMode: 1,
+      singlePageMode: 1,
+    };`;
+    script3.onload = this.initializeFlipbook; // Call initialization function once the script is loaded
+    document.head.appendChild(script3);
   }
 
-  initializeFlipbook() {
-    // JavaScript code to initialize the flipbook can go here
-    // Ensure the required libraries are properly loaded
-   
-  }
+//   initializeFlipbook() {
+//     // JavaScript code to initialize the flipbook can go here
+//     // Ensure the required libraries are properly loaded
+//     window.addEventListener('load', function () {
+//       const myFlipbook = new DFlip('._df_book', {
+//         source: 'location of pdf.pdf'
+//         // Additional configuration options here
+//       });
+//     });
+//   }
 
   render() {
     return (
       <div>
-        <div className="_df_book" id="flipbook" allControls="" pageMode='2' source="Print_DikshaUtkarsh.pdf"></div>
+        <div className="_df_book" id="flipbook" pageMode='2' source="Print_DikshaUtkarsh.pdf"></div>
       </div>
     );
   }
